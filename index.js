929da4245cc789bffa43e3288b1d497370b7e8f6
nelie250 <ntezielie@gmail.com>
2026-02-09 17:14:51 +0200

    require modeules

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+const ejs = require("ejs");
 const app = express();
+app.set("view engine", "ejs");
 
-const PORT = 3000;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(PORT,()=>console.log(`server is runnig on ${PORT} port`));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  }),
+);
+
+const categories = require("./routes/category.route.js");
+const userForm = require("./routes/user.route.js");
+const products = require("./routes/product.route.js");
+const suppliers = require("./routes/supplier.route.js");
+const inventory = require("./routes/inventory.route.js");
+const orders = require("./routes/order.route.js");
+const stock = require("./routes/stock.route.js");
+
+app.use("/", categories);
+app.use("/user", userForm);
+app.use("/products", products);
+app.use("/suppliers", suppliers);
+app.use("/inventory", inventory);
+app.use("/orders", orders);
+app.use("/stock-transactions", stock);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server is running on ${PORT} port`));
